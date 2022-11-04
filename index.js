e7642cd582fad7ffd60c561cbcfed33a57f88996
pjmarin <39600116+pjmarin@users.noreply.github.com>
2022-11-04 11:49:32 +0100

    Added jwt structure and links functionality

---
+++ index.js
@@ -0,0 +1,50 @@
+import "dotenv/config";
+import "./database/connectdb.js";
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.route.js";
+import linkRouter from "./routes/link.route.js";
+
+// (1) para servidor https
+// import https from "https";
+// import fs from "fs";
+
+const app = express();
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended: true, limit: '3mb'}))
+
+app.use(function(req, res, next) {
+  if(req.originalUrl.includes("api") || req.originalUrl.includes("html")) {
+    console.log(`pasamos por aqui - todas las peticiones - ${req.originalUrl} - ${new Date(Date.now()).toISOString()}`);
+  }
+  
+  next();
+});
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/links", linkRouter);
+
+app.use(express.static("public"));
+
+app.use(cors({ origin:true, credentials:true }))
+
+app.use(function(err, req, res, next) {
+  console.error("err.stack", err.stack);
+  return res.send(500, { message: err.message });
+});
+
+// (1) para servidor https
+// const options = {
+//   key: fs.readFileSync('key.pem'),
+//   cert: fs.readFileSync('cert.pem')
+// }
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log("😍😍😲 http://localhost:" + PORT));
+
+// (1) para servidor https
+// https.createServer(options, app).listen(PORT, console.log(`server runs on port ${PORT}`));
